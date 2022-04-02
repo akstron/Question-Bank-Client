c7dcafa5089c0c9c544c5c9d73aaffaab8460af0
Khushi Jain <khushij393@gmail.com>
2022-04-02 16:43:32 +0530

    loader

--- client/src/comp/navbar.js
+++ client/src/comp/navbar.js
@@ -9,10 +9,9 @@ const NavBar = () => {
             <Link to="/questions" className="nav__link">all questions</Link>
             <Link to="/addQuestion" className="nav__link">Add question</Link>
         </div>
-        <input type="text" placeholder="search"/>
+        
         <div className="nav__action">
-            <Link to="/login" className="nav__link nav__link--1">Login</Link>
-            <a href="/" className="nav__link nav__link--2">Sign up</a>
+            <Link to="/login" className="nav__link nav__link--1">Logout</Link>
         </div>
         </nav>
      );
